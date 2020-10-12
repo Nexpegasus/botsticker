483f759339f1a1bb48456a0bf2d78a35b49f00fa
Fandy / Nex Pegasus <62210459+Nexpegasus@users.noreply.github.com>
2020-10-12 15:01:24 +0700

    Update id.js

--- handler/message/text/id.js
+++ handler/message/text/id.js
@@ -25,7 +25,7 @@ exports.textMenu = (pushname) => {
 Hi, ${pushname}! 👋️
 Berikut adalah beberapa fitur yang ada pada bot ini!
 
-🌟Sticker Creator:
+🌟Sticker Creator :
 
 1. *#sticker*
 𝙼𝚎𝚗𝚐𝚞𝚋𝚊𝚑 𝚐𝚊𝚖𝚋𝚊𝚛 𝚖𝚎𝚗𝚓𝚊𝚍𝚒 𝚜𝚝𝚒𝚌𝚔𝚎𝚛.
@@ -34,12 +34,10 @@ Berikut adalah beberapa fitur yang ada pada bot ini!
 𝙼𝚎𝚗𝚐𝚞𝚋𝚊𝚑 𝚐𝚊𝚖𝚋𝚊𝚛 𝚍𝚊𝚛𝚒 𝚞𝚛𝚕.
 
 3. *#gifsticker* _<Giphy URL>_
-𝙼𝚎𝚗𝚐𝚞𝚋𝚊𝚑 𝚐𝚒𝚏 𝚖𝚎𝚗𝚓𝚊𝚍𝚒 𝚜𝚝𝚒𝚌𝚔𝚎𝚛 (𝚌𝚞𝚖𝚊 𝚏𝚘𝚛𝚖𝚊𝚝 𝚐𝚒𝚙𝚑𝚢).
-𝙲𝚘𝚗𝚝𝚘𝚑 𝚙𝚎𝚜𝚊𝚗 : *#gifsticker https://media.giphy.com/media/abcd/giphy.gif*.
+𝙼𝚎𝚗𝚐𝚞𝚋𝚊𝚑 format giphy 𝚖𝚎𝚗𝚓𝚊𝚍𝚒 𝚜𝚝𝚒𝚌𝚔𝚎𝚛
 
 4. *#meme* _<teks atas>_ | _<teks bawah>_
 𝚄𝚗𝚝𝚞𝚔 𝚖𝚎𝚖𝚋𝚞𝚊𝚝 𝚝𝚞𝚕𝚒𝚜𝚊𝚗 𝚊𝚝𝚊𝚜 𝚋𝚊𝚠𝚊𝚑 𝚙𝚊𝚍𝚊 𝚜𝚝𝚒𝚌𝚔𝚎𝚛 / 𝚐𝚊𝚖𝚋𝚊𝚛.
-𝙲𝚘𝚗𝚝𝚘𝚑 𝚙𝚎𝚜𝚊𝚗 : *#meme aku atas | kamu bawah*, 𝙰𝚝𝚊𝚞 𝚓𝚞𝚐𝚊 𝚋𝚒𝚜𝚊 𝚛𝚎𝚙𝚕𝚢 / 𝚖𝚎𝚖𝚋𝚊𝚕𝚊𝚜 𝚐𝚊𝚖𝚋𝚊𝚛 𝚢𝚊𝚗𝚐 𝚜𝚞𝚍𝚊𝚑 𝚊𝚍𝚊.
 
 🌟Downloader:
 
@@ -57,26 +55,23 @@ Penggunaan: kirim pesan dengan format *#ig https://www.instagram.com/p/CFHszv5DK
 
 4. *#twt* _<twitter post url>_
 Untuk mengunduh photo dan video dari Twitter.
-Penggunaan: kirim pesan dengan format *#twt https://twitter.com/../..*
+Penggunaan: kirim pesan dengan format *#twt https://twitter.com/user/status/...*
 
-🌟Lain-lain:
+🌟Lain-lain :
 
 1. *#translate* _<kode bahasa>_
-𝚄𝚗𝚝𝚞𝚔 𝚖𝚎𝚗𝚐𝚊𝚛𝚝𝚒𝚔𝚊𝚗 𝚋𝚊𝚑𝚊𝚜𝚊 𝚢𝚐 𝚍𝚒𝚝𝚎𝚗𝚝𝚞𝚔𝚊𝚗.
-Penggunaan: Balas/quote/reply pesan yang ingin kamu translate dengan _*#translate id*_ <- id adalah kode bahasa. kode bahasa dapat dilihat di *https://bit.ly/33FVldE*
+𝚄𝚗𝚝𝚞𝚔 𝚖𝚎𝚗𝚐𝚊𝚛𝚝𝚒𝚔𝚊𝚗 𝚋𝚊𝚑𝚊𝚜𝚊.
+Penggunaan: Balas, Repy/Quote Pesan yg ingin di translate dengan format #translate <kode negara> "#translate id"
+Kode Negara ? Ketik /kodenegara
 
 2. *#resi* _<kurir>_ _<nomer resi>_
-𝚄𝚗𝚝𝚞𝚔 𝚖𝚎𝚖𝚎𝚛𝚒𝚔𝚜𝚊 𝚜𝚝𝚊𝚝𝚞𝚜 𝚙𝚎𝚗𝚐𝚒𝚛𝚒𝚖𝚊𝚗 𝚋𝚊𝚛𝚊𝚗𝚐, 𝚍𝚊𝚏𝚝𝚊𝚛 𝚔𝚞𝚛𝚒𝚛 : 𝚓𝚗𝚎, 𝚙𝚘𝚜, 𝚝𝚒𝚔𝚒, 𝚠𝚊𝚑𝚊𝚗𝚊, 𝚓𝚗𝚝, 𝚛𝚙𝚡, 𝚜𝚊𝚙, 𝚜𝚒𝚌𝚎𝚙𝚊𝚝, 𝚙𝚌𝚙, 𝚓𝚎𝚝, 𝚍𝚜𝚎, 𝚏𝚒𝚛𝚜𝚝, 𝚗𝚒𝚗𝚓𝚊, 𝚕𝚒𝚘𝚗, 𝚒𝚍𝚕, 𝚛𝚎𝚡.
-Penggunaan: kirim pesan dengan format _*#resi jne 1238757632*_
+𝚄𝚝𝚔 𝚖𝚎𝚖𝚎𝚛𝚒𝚔𝚜𝚊 𝚜𝚝𝚊𝚝𝚞𝚜 𝚙𝚎𝚗𝚐𝚒𝚛𝚒𝚖𝚊𝚗 𝚋𝚊𝚛𝚊𝚗𝚐.
+Penggunaan: kirim pesan seperti *#resi jne 1238757632*
 
-3. *#ceklokasi*
-𝙲𝚎𝚔 𝚕𝚘𝚔𝚊𝚜𝚒 𝚙𝚎𝚗𝚢𝚎𝚋𝚊𝚛𝚊𝚗 𝚌𝚘𝚟𝚒𝚍-𝟷𝟿 𝚍𝚒 𝚍𝚊𝚎𝚛𝚊𝚑 𝚜𝚎𝚔𝚒𝚝𝚊𝚛𝚖𝚞 (𝚔𝚎𝚕𝚞𝚛𝚊𝚑𝚊𝚗).
-Penggunaan: kirimkan lokasimu lalu balas/quote/reply lokasi yang kamu kirim dengan _*#ceklokasi*_
-
-4. *#tnc*
+3. *#tnc*
 𝙼𝚎𝚗𝚊𝚖𝚙𝚒𝚕𝚔𝚊𝚗 𝚜𝚢𝚊𝚛𝚊𝚝 𝚍𝚊𝚗 𝚔𝚘𝚗𝚍𝚒𝚜𝚒 𝚋𝚘𝚝.
 
-5. *#donasi*
+4. *#donasi*
 𝙼𝚎𝚗𝚊𝚖𝚙𝚒𝚕𝚔𝚊𝚗 𝚒𝚗𝚏𝚘𝚖𝚊𝚜𝚒 𝚍𝚘𝚗𝚊𝚜𝚒
 
 𝙃𝙤𝙥𝙚 𝙮𝙤𝙪 𝙝𝙖𝙫𝙚 𝙖 𝙜𝙧𝙚𝙖𝙩 𝙙𝙖𝙮!✨`
@@ -103,6 +98,7 @@ Untuk menghapus pesan bot (balas pesan bot dengan #del)`
 exports.textDonasi = () => {
     return `
 Hai, terimakasih telah menggunakan bot ini, dukung saya di link berikut:
+
 1. Saweria : https://saweria.co/fandystore
 
 Donasi akan digunakan untuk pengembangan dan pengoperasian bot ini.
