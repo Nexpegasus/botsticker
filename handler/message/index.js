57bed11b9577a69e577cc4b3412336d3dae4b0c5
Fandy / Nex Pegasus <62210459+Nexpegasus@users.noreply.github.com>
2020-10-12 15:04:38 +0700

    Update index.js

--- handler/message/index.js
+++ handler/message/index.js
@@ -78,8 +78,7 @@ module.exports = msgHandler = async (client = new Client(), message) => {
                 const mediaData = await decryptMedia(encryptMedia, uaOverride)
                 const imageBase64 = `data:${_mimetype};base64,${mediaData.toString('base64')}`
                 client.sendImageAsSticker(from, imageBase64).then(() => {
-                    client.reply(from, 'Ni Stikernya Woi :)')
-                    client.reply(from, `Sticker Processed for ${processTime(t, moment())} Second`)
+                    client.reply(from, 'Stickernya Kaka :), Prosesnya ${processTime(t, moment())} Detik')
                     console.log(`Sticker Processed for ${processTime(t, moment())} Second`)
                 })
             } else if (args[0] === 'nobg') {
